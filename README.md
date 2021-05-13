c18db7475925c5b28c9dcc624f6c734e06aee8f9
Simon Davis <sdavis@hashicorp.com>
2021-05-12 17:05:21 -0700

    locally working

---
+++ README.md
@@ -0,0 +1,3 @@
+# GitHub Project Archive
+
+Given a release which is named the same as a milestone, this action will archive any cards in a specified column which belong to a specified release/milestone. Intended to be used from a release event.
---
+++ index.js
@@ -0,0 +1,68 @@
+const core = require('@actions/core');
+const { Octokit } = require('@octokit/action');
+
+const GITHUB_DONE_COLUMN_ID = core.getInput('github_done_column_id');
+const GITHUB_RELEASE_NAME = core.getInput('github_release_name');
+
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+
+async function main() {
+  const octokit = new Octokit();
+
+  let cards = [];
+
+  try {
+    const response = await octokit.request('GET /projects/columns/{column_id}/cards', {
+      column_id: GITHUB_DONE_COLUMN_ID,
+      mediaType: {
+        previews: [
+          'inertia',
+        ],
+      },
+    });
+    cards = response.data;
+    core.info(`Found ${cards.length} cards in done column`);
+  } catch (error) {
+    core.setFailed('Error retrieving project cards in done column');
+  }
+
+  for (let index = 0; index < cards.length; index += 1) {
+    const card = cards[index];
+    if (card.content_url) {
+      const urlSegments = card.content_url.split('/');
+      const issueNumber = urlSegments[urlSegments.length - 1];
+      let issue = {};
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const issueReponse = await octokit.rest.issues.get({
+          owner,
+          repo,
+          issueNumber,
+        });
+        issue = issueReponse.data;
+      } catch (error) {
+        core.setFailed(`Error retrieving issue from card ${error}`);
+      }
+
+
+      if (issue.milestone.title === GITHUB_RELEASE_NAME) {
+        core.info(`Issue ${issue.number} has been released`);
+        try {
+          // eslint-disable-next-line no-await-in-loop
+          await octokit.rest.projects.updateCard({
+            card_id: card.id,
+            archived: true,
+          });
+        } catch (error) {
+          core.setFailed(`Error archiving card ${error}`);
+        }
+      }
+    }
+  }
+}
+
+try {
+  main();
+} catch (error) {
+  core.setFailed(error);
+}
